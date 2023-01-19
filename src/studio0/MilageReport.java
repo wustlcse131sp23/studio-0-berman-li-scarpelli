ef4f205a3a54add8d85568eaeac06bd09ec4fa87
Judyli <Judyli@localhost>
2023-01-19 12:08:27 -0600

    Finished up to Message

--- src/studio0/MilageReport.java
+++ src/studio0/MilageReport.java
@@ -8,8 +8,8 @@ public class MilageReport {
 		int car3miles = 900;
 		
 		int totalMiles = car1miles + car2miles + car3miles;
-		double milesPerGallon = 24.5;
-		double totalGallons = totalMiles * milesPerGallon;
+		double gallonsPerMile = 1/24.5;
+		double totalGallons = totalMiles * gallonsPerMile;
 		double pricePerGallon = 3.05;
 		double totalPrice = totalGallons * pricePerGallon;
 		
